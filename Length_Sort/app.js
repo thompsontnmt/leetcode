badde7848ae461c3e399fa60c385fe769de78a37
Mike Thompson <mtthompson11@gmail.com>
2022-11-26 14:51:14 -0600

    length sort

---
+++ Length_Sort/app.js
@@ -0,0 +1,23 @@
+var text = "Length is what you want";
+var reOrder = function(text) {
+    const words = text.toLowerCase().split(' ').map((val, i) => {
+        return {
+            text: val,
+            length: val.length,
+            index: i
+        }
+    });
+    console.log(words);
+    words.sort((a, b) => {
+        return a.length - b.length;
+    });
+    let i = 0;
+    let result = '';
+    while (i < words.length - 1) {
+        result += words[i++].text + ' ';
+    }
+    result += words[i].text;
+    return result[0].toUpperCase() + result.substring(1);
+    console.log(result);
+}
+reOrder(text);
