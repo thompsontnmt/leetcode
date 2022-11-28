0ecd136a7cc091c2c67cae1340b33c2b03922ccc
Mike Thompson <mtthompson11@gmail.com>
2022-11-27 21:06:41 -0600

    preorder traversal function

--- pre_order_binary/app.js
+++ pre_order_binary/app.js
@@ -1,3 +1,26 @@
 var preOrderTraversal = function(root) {
-    
+    if (root == null) {
+        return [];
+    }
+    if (root.left == null && root.right == null) {
+        return [root.val];
+    }
+    var res = [];
+    var nodes = [root];
+    var tempNodes = [];
+    while (nodes.length > 0) {
+        while (nodes.length > 0) {
+            var node = nodes.pop();
+            res.push(node.val);
+            if (node.right) {
+                tempNodes.push(node.right);
+            }
+            if (node.left) {
+                tempNodes.push(node.left);
+            }
+        }
+        nodes = tempNodes;
+    }
+    return res;
 }
+preOrderTraversal(root);
