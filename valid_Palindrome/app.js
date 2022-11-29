54bb5670220503b8779a0dacc1e346bdc4068a7e
Mike Thompson <mtthompson11@gmail.com>
2022-11-28 20:00:41 -0600

    isPalindrome

---
+++ valid_Palindrome/app.js
@@ -0,0 +1,11 @@
+// Leetcode 125
+
+var s = "A man, a plan, a canal: Panama";
+var isPalindrom = (s) => {
+    if (s.length <= 1) return true;
+
+    let [L, R] = [0, s.length - 1];
+    let leftChar, rightChar;
+}
+
+isPalindrom(s);
