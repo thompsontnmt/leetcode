1ff7129906a5a3561f701042fb8f67628acbcfb0
Mike Thompson <mtthompson11@gmail.com>
2022-11-28 19:19:15 -0600

    valid anagram js function

--- validAnagram/app.js
+++ validAnagram/app.js
@@ -1,5 +1,15 @@
+var s = "anagram";
+var t = "nagaram";
 var validAnagram = (s, t) => {
+    if (s.length !== t.length) return false;
 
-}
+    return reorder(s) === reorder(t);
+
+};
+
+const reorder = (str) => str
+    .split('')
+    .sort((a, b) => a.localeCompare(b))
+    .join('');
 
 validAnagram(s, t);
