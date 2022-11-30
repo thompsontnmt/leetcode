63fcf19cd28ed55e9a2f5d3c56ebee244cd1ba2d
Mike Thompson <mtthompson11@gmail.com>
2022-11-29 20:42:28 -0600

    BuySellStock2

--- best_time_to_sell/app.js
+++ best_time_to_sell/app.js
@@ -1,7 +1,18 @@
 var maxProfit = (prices) => {
     if (prices.length < 2) return 0;
 
-    let min = 0;
-    let max = prices[0];
-    
+    let min_Profit = prices[0];
+    let max_Profit = prices[1] - prices[0];
+
+    for (day in prices) {
+        let current_price = prices[day];
+
+        tentative_max_profit = current_price - min_Profit;
+
+        max_Profit = max(max_Profit, tentative_max_profit);
+
+        min_Profit = min(min_Profit, current_price);
+    }
+    if (max_Profit > 0) return max_Profit;
+    return 0;
 }
