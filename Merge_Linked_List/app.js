c30c82397dd44e63ff88f2c29d401c03bcba6b84
Mike Thompson <mtthompson11@gmail.com>
2022-11-29 22:02:04 -0600

    mergeLLNotRight

--- Merge_Linked_List/app.js
+++ Merge_Linked_List/app.js
@@ -7,7 +7,15 @@ var mergeTwoLists = (list1, list2) => {
         if (l1.val < l2.val) {
             p.next = l1;
             l1 = l1.next;
-            
+            p = p.next
+        } else {
+            p.next = l2;
+            l2 = l2.next;
+            p = p.next
         }
     }
+    if (l1) p.next = l1;
+    if (l2) p.next = l2;
+
+    return dummy.next;
 }
